5091e4fe6164baa8d3803143657e8b29a51b2510
rnaqpddl123@gmail.com <rnaqpddl123@gmail.com>
2022-12-07 17:04:22 +0900

    Servlet

---
+++ src/main/java/ch05/ThirdServlet.java
@@ -0,0 +1,52 @@
+package ch05;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ThirdServlet
+ */
+@WebServlet("/ch05/third")
+public class ThirdServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.print("<!DOCTYPE html>");
+		out.print("<html>");
+		out.print("<head>");
+		out.print("<meta charset=\"UTF-8\">");
+		out.print("<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">");
+		out.print("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+		out.print("<title>Insert title here</title>");
+		out.print("</head>");
+		out.print("<body>");
+		out.print("<h1>Chapter 04</h1>");
+		out.print("<h1>Hello world!!</h1>");
+		out.print("<h1>자바웹</h1>");
+		out.print("</body>");
+		out.print("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
